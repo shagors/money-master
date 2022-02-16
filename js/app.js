b896521ae0d33946d945fcd8ea5b97e6a0df36f1
shagors <shajjadshagor@gmail.com>
2022-02-17 01:16:44 +0600

    universal function added and its works properly

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+// universal function for income and expenses
+
 function savingsBalance(balanceInputField) {
-    const incomeFieldGet = document.getElementById(balanceInputField);
-    const incomeFieldText = incomeFieldGet.value;
-    const incomeField = parseFloat(incomeFieldText);
-    return incomeField;
+    const balanceFieldGet = document.getElementById(balanceInputField);
+    const balanceFieldText = balanceFieldGet.value;
+    const balanceField = parseFloat(balanceFieldText);
+    balanceFieldGet.value = '';
+    return balanceField;
 }
 
 
@@ -11,38 +14,28 @@ function savingsBalance(balanceInputField) {
 document.getElementById('total-calculate').addEventListener('click', function () {
 
     // income field
-    // const incomeFieldGet = document.getElementById('income-field');
-    // const incomeFieldText = incomeFieldGet.value;
-    // const incomeField = parseFloat(incomeFieldText);
-    savingsBalance('income-field');
-
+    const incomeField = savingsBalance('income-field');
 
     // Expenses field
 
     // food
-    const foodCostFieldGet = document.getElementById('food-expenses');
-    const foodCostFieldText = foodCostFieldGet.value;
-    const foodCostField = parseFloat(foodCostFieldText);
+    const foodExpenses = savingsBalance('food-expenses');
 
     // rent 
-    const rentCostFieldGet = document.getElementById('rent-expenses');
-    const rentCostFieldText = rentCostFieldGet.value;
-    const rentCostField = parseFloat(rentCostFieldText);
+    const rentExpenses = savingsBalance('rent-expenses');
 
     // clothes
-    const clothesCostFieldGet = document.getElementById('clothes-expenses');
-    const clothesCostFieldText = clothesCostFieldGet.value;
-    const clothesCostField = parseFloat(clothesCostFieldText);
+    const clothesExpenses = savingsBalance('clothes-expenses');
 
     // food + rent + clothes = total expenses\
     const totalExpensesGet = document.getElementById('total-expenses');
     const totalExpensesSet = totalExpensesGet.innerText;
 
-    let totalExpenses = foodCostField + rentCostField + clothesCostField;
+    let totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
     totalExpensesGet.innerText = totalExpenses;
 
     // balance remain after expenses
-    const remainBalance = savingsBalance('income-field') - totalExpenses;
+    const remainBalance = incomeField - totalExpenses;
     
     const balanceAfterExpensesGet = document.getElementById('total-balance');
     const balanceAfterExpensesSet = balanceAfterExpensesGet.innerText;
