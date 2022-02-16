82ca87bc60e8981e40a2eeaed537953a25206213
shagors <shajjadshagor@gmail.com>
2022-02-17 01:01:05 +0600

    1st syep complete total expenses and balance done all works properly

--- js/app.js
+++ js/app.js
@@ -1,10 +1,52 @@
+function savingsBalance(balanceInputField) {
+    const incomeFieldGet = document.getElementById(balanceInputField);
+    const incomeFieldText = incomeFieldGet.value;
+    const incomeField = parseFloat(incomeFieldText);
+    return incomeField;
+}
+
+
 // calculate button work
 
 document.getElementById('total-calculate').addEventListener('click', function () {
+
     // income field
-    const incomeFieldText = document.getElementById('income-field').value;
-    const incomeField = parseFloat(incomeFieldText);
-    console.log(incomeField);
+    // const incomeFieldGet = document.getElementById('income-field');
+    // const incomeFieldText = incomeFieldGet.value;
+    // const incomeField = parseFloat(incomeFieldText);
+    savingsBalance('income-field');
+
+
+    // Expenses field
+
+    // food
+    const foodCostFieldGet = document.getElementById('food-expenses');
+    const foodCostFieldText = foodCostFieldGet.value;
+    const foodCostField = parseFloat(foodCostFieldText);
+
+    // rent 
+    const rentCostFieldGet = document.getElementById('rent-expenses');
+    const rentCostFieldText = rentCostFieldGet.value;
+    const rentCostField = parseFloat(rentCostFieldText);
+
+    // clothes
+    const clothesCostFieldGet = document.getElementById('clothes-expenses');
+    const clothesCostFieldText = clothesCostFieldGet.value;
+    const clothesCostField = parseFloat(clothesCostFieldText);
+
+    // food + rent + clothes = total expenses\
+    const totalExpensesGet = document.getElementById('total-expenses');
+    const totalExpensesSet = totalExpensesGet.innerText;
+
+    let totalExpenses = foodCostField + rentCostField + clothesCostField;
+    totalExpensesGet.innerText = totalExpenses;
+
+    // balance remain after expenses
+    const remainBalance = savingsBalance('income-field') - totalExpenses;
+    
+    const balanceAfterExpensesGet = document.getElementById('total-balance');
+    const balanceAfterExpensesSet = balanceAfterExpensesGet.innerText;
+    balanceAfterExpensesGet.innerText = remainBalance;
 
 
 });
