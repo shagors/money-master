19cff0575bd60b751c66b0e5e5a76d471a3b4c2e
shagors <shajjadshagor@gmail.com>
2022-02-16 23:32:30 +0600

    income field properly work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log("i am ok");
+// calculate button work
+
+document.getElementById('total-calculate').addEventListener('click', function () {
+    // income field
+    const incomeFieldText = document.getElementById('income-field').value;
+    const incomeField = parseFloat(incomeFieldText);
+    console.log(incomeField);
+
+
+});
